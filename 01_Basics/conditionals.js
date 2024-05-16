4515c2b311f6fc7371af73da50e2a812fd22733a
ChinnariChetanaPatro <124259413+ChinnariChetanaPatro@users.noreply.github.com>
2024-05-16 02:45:38 +0000

    conditionals

---
+++ 01_Basics/conditionals.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2!=1);
+
+// console.log("2">1);  //here js autumatically converts "2" string to integer
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+console.log(2=="2");
+console.log(2==="2");  // === is known as strict check. It checks the datatypes also and then give the result
+
