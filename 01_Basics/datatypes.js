cfe47d14edaf17c4e00349edd2826a61ade63c77
ChinnariChetanaPatro <124259413+ChinnariChetanaPatro@users.noreply.github.com>
2024-05-16 01:17:52 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,17 @@
+let name = "ccp"
+let age = 18
+let loggedIn = true
+let accountId
+
+//int
+//bigInt
+//string = ""
+//boolean = true or false
+//null
+//undefined
+//symbol
+
+console.log(typeof"ccp");
+console.log(typeof age);
+console.log(typeof null);      //Null is an object 
+console.log(typeof undefined); //type is undefined
