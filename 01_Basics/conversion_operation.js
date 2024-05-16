4212f056706272bfa6633b39edee4dfb6c30b694
ChinnariChetanaPatro <124259413+ChinnariChetanaPatro@users.noreply.github.com>
2024-05-16 02:11:18 +0000

    operations

--- 01_Basics/conversion_operation.js
+++ 01_Basics/conversion_operation.js
@@ -1,30 +1,64 @@
-let score = null
+// let score = null
 
 
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log(valueInNumber);        
-console.log(typeof valueInNumber);
-console.log(typeof (valueInNumber));
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);        
+// console.log(typeof valueInNumber);
+// console.log(typeof (valueInNumber));
 
-//number convert
-//33- can be converted
-//33abc - can't be converted - NaN
-// null = 0
-//undefined = NaN
+// //number convert
+// //33- can be converted
+// //33abc - can't be converted - NaN
+// // null = 0
+// //undefined = NaN
 
 
-let isLoggedIn = ""
-let loggedInValue = Boolean(isLoggedIn);
-console.log(loggedInValue);
+// let isLoggedIn = ""
+// let loggedInValue = Boolean(isLoggedIn);
+// console.log(loggedInValue);
 
-//1 = true, 0=false
-//""=false "ccp"=true
+// //1 = true, 0=false
+// //""=false "ccp"=true
 
-let sum = 33
-let stringSum = String(sum)
-console.log(stringSum);
-console.log(typeof(stringSum));
+// let sum = 33
+// let stringSum = String(sum)
+// console.log(stringSum);
+// console.log(typeof(stringSum));
+
+// ****************************OPERATIONS******************************
+// let value = 3;
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2%2);
+
+// let str1 ="hello"
+// let str2 = " ccp"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
