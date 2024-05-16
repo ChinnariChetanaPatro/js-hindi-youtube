d42f62c773f03bcb5279dcc5122e69e9fc0831c0
ChinnariChetanaPatro <124259413+ChinnariChetanaPatro@users.noreply.github.com>
2024-05-16 02:45:24 +0000

    changes to datatypes

--- 01_Basics/datatypes.js
+++ 01_Basics/datatypes.js
@@ -9,9 +9,30 @@ let accountId
 //boolean = true or false
 //null
 //undefined
-//symbol
+//symbol // to make some unique
 
 console.log(typeof"ccp");
 console.log(typeof age);
 console.log(typeof null);      //Null is an object 
 console.log(typeof undefined); //type is undefined
+
+
+
+/* #Primitive (call by value)
+    7 categories :  String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+    #Reference/ Non primitive(call by reference)
+Array, Object, Functions
+
+Java Script is dynamically typed language
+
+    */
+
+const heroes = ["Shaktiman", "Nagraj"]
+let myObj = {
+    name: "ccp",
+    age: 22
+}
+const myFunction = function(){
+    console.log("Hello world");
+}
