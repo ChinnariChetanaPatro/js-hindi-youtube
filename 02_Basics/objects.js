b4e87cd349c1b406fa08d4bc38120c31c7919b0c
Chinnari Chetana Patro <124259413+ChinnariChetanaPatro@users.noreply.github.com>
2024-05-17 16:44:15 +0000

    objects continue

---
+++ 02_Basics/objects.js
@@ -0,0 +1,34 @@
+// singleton Object.create
+// if object is made from constructor it will be a singleton itherwise not
+
+
+// myArray = ["c", "c", "p"]
+// myArray[0] //accessed by index i.e key can't be defined only values can be defined
+//object literals
+//in object both keys and values can be defined
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "ccp",
+    // 0: "chetna",
+    "full name":"Chinnari Chetana Patro",
+    //to access such values console.log(JsUser["full name"]); method is used
+    // mySym: "myKey1", //it's a string rn not a key 
+    [mySym]: "myKey1",  //use sq bracket for using as a symbol
+    location:"bhubaneswar",
+    email: "ccp@google.com",
+    isLoggedin: false,              //boolean
+    lastLogIn: ["mon","sat"]        //array
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+console.log(typeof(JsUser.mySym)); 
+
+JsUser.email = "ccp@chatgpt.com"  //overwriting the value
+Object.freeze(JsUser)
+JsUser.email = "ccp@microsoft.com"
+console.log(JsUser);
